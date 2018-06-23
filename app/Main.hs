d9da9591810b17938adeb6f105fd5ace0aecad20
Hexirp <Hexirp@users.noreply.github.com>
2018-06-23 20:22:11 +0900

    Add: Hakyllが使うフォルダの設定を

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,12 @@
 module Main where
  import Prelude
 
+ import Hakyll
+
  main :: IO ()
  main = return ()
+
+ hakyllConfig :: Configuration
+ hakyllConfig = def {
+  providerDirectory = "docs-pre",
+  destinationDirectory = "docs"}
