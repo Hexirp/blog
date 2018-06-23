f476d28ca14a5308e7292ed294bf18eeedff8200
Hexirp <Hexirp@users.noreply.github.com>
2018-06-23 20:56:49 +0900

    Add: articlesRuleを

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
  import Prelude
 
  import Hakyll
 
  main :: IO ()
- main = hakyllWith config $ return ()
+ main = hakyllWith config $ articlesRule
 
  config :: Configuration
  config = defaultConfiguration {
   providerDirectory = "docs-pre",
   destinationDirectory = "docs"}
+
+ articlesRule :: Rules ()
+ articlesRule = match "articles/*" $ do
+  route $ setExtension "html"
+  compile pandocCompiler
