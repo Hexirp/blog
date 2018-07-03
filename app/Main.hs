09683be73bc2ca21d47b9193bba96f3c1e5aabc4
Hexirp <Hexirp@users.noreply.github.com>
2018-07-03 15:37:12 +0900

    Fix: パスの区切りを互換に
    指定したパスは検索のときにそのまま使われるので
    Windowsで実行するとファイルが見つからなくなる

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@
 module Main where
  import Prelude
 
+ import System.FilePath ((</>))
+
  import Hakyll
 
  main :: IO ()
@@ -17,14 +19,14 @@ module Main where
   destinationDirectory = "docs"}
 
  templatesRule :: Rules ()
- templatesRule = match "templates/*" $ compile templateCompiler
+ templatesRule = match "templates" </> "*" $ compile templateCompiler
 
  stylesRule :: Rules ()
- stylesRule = match "styles/*" $ do
+ stylesRule = match "styles" </> "*" $ do
   route idRoute
   compile $ compressCssCompiler
 
  articlesRule :: Rules ()
- articlesRule = match "articles/*" $ do
+ articlesRule = match "articles" </> "*" $ do
   route $ setExtension "html"
   compile pandocCompiler
